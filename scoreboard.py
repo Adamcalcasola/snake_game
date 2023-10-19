a527f70036f944c4b5bcac89b80829168667564f
Adam Calcasola <adam.calcasola@gmail.com>
2023-10-19 16:05:57 -0400

    finished project

--- scoreboard.py
+++ scoreboard.py
@@ -14,9 +14,13 @@ class Scoreboard(Turtle):
         self.display_score()
 
     def display_score(self):
-        self.write(f"Score: {self.score}", MOVE,ALIGNMENT, FONT)
+        self.write(f"Score: {self.score}", MOVE, ALIGNMENT, FONT)
 
     def add_point(self):
         self.score += 1
         self.clear()
         self.display_score()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", MOVE, ALIGNMENT, FONT)
